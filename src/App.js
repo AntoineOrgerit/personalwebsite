73bf0e3061967c926220a55ac5ef76d63fba2b93
Antoine Orgerit <orgerit.antoine@gmail.com>
2020-07-29 13:27:00 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+/**
+ * Copyright 2020 Antoine Orgerit. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Layout } from 'antd';
+
+const Content = Layout.Content;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout id="global-container">
+        {/*navbar goes here*/}
+        <Content>
+          <Switch>
+            <Route path="*">
+              <p>No content yet.</p>
+            </Route>
+          </Switch>
+        </Content>
+        {/*footer goes here*/}
+      </Layout>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+/**
+ * Copyright 2020 Antoine Orgerit. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import './reset.css';
+import './index.css';
+import './theme/theme.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +23,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
