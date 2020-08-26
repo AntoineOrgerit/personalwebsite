093c7a7f1b9b4321d38edf6a2aac6941bc24659b
Antoine Orgerit <orgerit.antoine@gmail.com>
2020-08-26 15:21:06 +0100

    feat: adding translations for home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Typography, Button, Row, Col } from 'antd';
 import TextLoop from "react-text-loop";
 import ScrollAnimation from 'react-animate-on-scroll';
@@ -15,7 +16,7 @@ import ProgrammingCityBackground from './resources/img/programming_city_edit.jpg
 
 import './Home.css';
 
-const { Paragraph, Title } = Typography;
+const { Paragraph, Title, Text } = Typography;
 
 /**
  * Home page of the website.
@@ -24,17 +25,21 @@ const { Paragraph, Title } = Typography;
  * @version 1.0
  */
 export default function Home() {
+    const { t } = useTranslation();
+
     return <div className="home-container">
         <FullscreenParallax backgroundImage={ProgrammingCityBackground} topOffset={0.25} bottomOffset={0.2} height={(document.body.clientHeight - 64) * 0.9}>
             <Paragraph>Antoine Orgerit</Paragraph>
-            <Title level={2}>Software Engineering Graduate</Title>
+            <Title level={2}>{t("home.title")}</Title>
         </FullscreenParallax>
         <div className="skills-carousel-container">
             <Title className="word-carousel" level={3}>
-                Knowledge in: <TextLoop className="word-carousel-items" mask={true}>
-                    <span>Java</span>
-                    <span>JavaScript</span>
-                    <span>ReactJS</span>
+                {t("home.skillsHighlight.heading")} <TextLoop className="word-carousel-items" mask={true}>
+                    {
+                        t("home.skillsHighlight.items", { returnObjects: true }).map((skill, index) => (
+                            <span key={index}>{skill}</span>
+                        ))
+                    }
                 </TextLoop>
             </Title>
         </div>
@@ -42,9 +47,11 @@ export default function Home() {
             <Row gutter={[60, 0]}>
                 <Col md={16}>
                     <div className="home-item-content">
-                        <Title level={3}>Combining development and key values</Title>
-                        <Paragraph>As a Software Engineering graduate, software development is in the heart of my everyday life point of view. From Front End to Back End, I try to put my work under sustainability, quality and reusability principles. This aims to emphasize evolution, either of the project itself or concepts and guidelines.</Paragraph>
-                        <Button>See more</Button>
+                        <Title level={3}>{t("home.developerSection.heading")}</Title>
+                        <Paragraph>{t("home.developerSection.content")}</Paragraph>
+                        <div className="link-container">
+                            <Button href="/about">{t("home.developerSection.button")}</Button>
+                        </div>
                     </div>
                 </Col>
                 <Col md={8}>
@@ -59,9 +66,11 @@ export default function Home() {
                 </Col>
                 <Col md={16}>
                     <div className="home-item-content">
-                        <Title level={3}>Varied experience and interest</Title>
-                        <Paragraph>Experience is what value, through competencies and subjects. My professional experience reflects my curiousity about most topics of information technology (public or private interests, research, practices...). Still, I am mostly oriented to Full Stack stakes: data modeling, web services exposure, web or mobile front end solutions...</Paragraph>
-                        <Button>See more</Button>
+                        <Title level={3}>{t("home.experienceSection.heading")}</Title>
+                        <Paragraph>{t("home.experienceSection.content")}</Paragraph>
+                        <div className="link-container">
+                            <Button href="/experience">{t("home.experienceSection.button")}</Button>
+                        </div>
                     </div>
                 </Col>
             </Row>
@@ -70,9 +79,11 @@ export default function Home() {
             <Row gutter={[60, 0]}>
                 <Col md={16}>
                     <div className="home-item-content">
-                        <Title level={3}>Continuously learning, with projects</Title>
-                        <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae quam non erat ultrices mattis ac a ex. Integer sed posuere felis. Cras nec consequat quam. Nullam efficitur tristique arcu, id lobortis ipsum commodo quis. Etiam dictum, sem ullamcorper maximus volutpat, quam magna iaculis nibh, vel pellentesque urna ex ut erat. Sed eget nunc cursus, suscipit tortor non, faucibus ante. Quisque dapibus, felis ut auctor congue, turpis erat venenatis justo, nec ultricies ipsum sapien at turpis.</Paragraph>
-                        <Button>See more</Button>
+                        <Title level={3}>{t("home.projectsSection.heading")}</Title>
+                        <Paragraph>{t("home.projectsSection.content")}</Paragraph>
+                        <div className="link-container">
+                            <Button href="/projects">{t("home.projectsSection.button")}</Button>
+                        </div>
                     </div>
                 </Col>
                 <Col md={8}>
