2be676daad989649c6b197fbb8a52cd8a8c6dafb
Antoine Orgerit <orgerit.antoine@gmail.com>
2020-08-26 09:55:24 +0100

    feat: updating footer with small contact icons

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -8,17 +8,19 @@ import React from 'react';
 import { Layout, Typography, Row, Col } from 'antd';
 
 import './Footer.css';
-import { LinkedinFilled, GithubFilled, MailOutlined, PhoneOutlined } from '@ant-design/icons';
+import { LinkedinFilled, GithubFilled, MailOutlined, PhoneOutlined, GitlabOutlined, WhatsAppOutlined } from '@ant-design/icons';
 
 const { Text } = Typography;
 
 export default function Footer() {
     return <Layout.Footer id="footer">
         <Text className="footer-contact-container"><Row justify="space-around" gutter={[16, 0]}>
-            <Col className="footer-contact-item" sm><a href="https://www.linkedin.com/in/antoine-orgerit/" target="_blank" rel="noopener noreferrer"><LinkedinFilled />&nbsp;antoine-orgerit</a></Col>
-            <Col className="footer-contact-item" sm><a href="https://github.com/AntoineOrgerit" target="_blank" rel="noopener noreferrer"><GithubFilled />&nbsp;AntoineOrgerit</a></Col>
-            <Col className="footer-contact-item" sm><a href="mailto:orgerit.antoine@gmail.com"><MailOutlined />&nbsp;orgerit.antoine@gmail.com</a></Col>
-            <Col className="footer-contact-item" sm><a href="tel:+33678081825"><PhoneOutlined />&nbsp;+33&nbsp;(0)6&nbsp;78&nbsp;08&nbsp;18&nbsp;25</a></Col>
+            <Col className="footer-contact-item" sm><a href="https://www.linkedin.com/in/antoine-orgerit/" target="_blank" rel="noopener noreferrer"><LinkedinFilled /></a></Col>
+            <Col className="footer-contact-item" sm><a href="https://github.com/AntoineOrgerit" target="_blank" rel="noopener noreferrer"><GithubFilled /></a></Col>
+            <Col className="footer-contact-item" sm><a href="https://gitlab.com/AntoineOrgerit" target="_blank" rel="noopener noreferrer"><GitlabOutlined /></a></Col>
+            <Col className="footer-contact-item" sm><a href="mailto:orgerit.antoine@gmail.com"><MailOutlined /></a></Col>
+            <Col className="footer-contact-item" sm><a href="tel:+33678081825"><PhoneOutlined /></a></Col>
+            <Col className="footer-contact-item" sm><a href="https://wa.me/33678081825" target="_blank" rel="noopener noreferrer"><WhatsAppOutlined /></a></Col>
         </Row></Text>
         <Text type="secondary">&copy; Antoine Orgerit, 2020. All rights reserved.</Text>
     </Layout.Footer>;
